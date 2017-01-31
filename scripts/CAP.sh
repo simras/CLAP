e751bf77defa42fe471ef9ec8b104dc2e7ace392
simras <simras@binf.ku.dk>
2017-01-31 16:26:36 +0100

    update CAP.sh

--- scripts/CAP.sh
+++ scripts/CAP.sh
@@ -138,128 +138,118 @@ L=1024
 # Error prob binomial model see original BWA paper
 E=0.04
 
+error_model=resources/error_model_125.txt
+
 # Filenames 
 file_list=$1
 FILE=$(basename $file_list)
 FILE_NAME=$(dirname $file_list)
-#for FILE in $file_list
-#do
     
-    if [ $6 -eq 2 -o $6 -eq 4 ]
+if [ $6 -eq 2 -o $6 -eq 4 ]
+then
+    if [ "$barcode" == "" ]
     then
-	if [ "$barcode" == "" ]
-	then
-	    barcode="nobc"
-	fi
+	barcode="nobc"
     fi
-    ftmp=$barcode"_assigned_"$FILE
-    f2=$barcode"_noADPT_"$FILE
-    f3=$barcode"_noDUPS_"$FILE
-    final_bed=$barcode"_merged_"${FILE%.*}.bed
-    clusters=$barcode"_"${FILE%.*}"_clusters".bed
+fi
+ftmp=$barcode"_assigned_"$FILE
+f2=$barcode"_noADPT_"$FILE
+f3=$barcode"_noDUPS_"$FILE
+final_bed=$barcode"_merged_"${FILE%.*}.bed
+clusters=$barcode"_"${FILE%.*}"_clusters".bed
+
+if [ $6 -eq 1 -o $6 -eq 3 ]
+then
+    # Remove barcode and select data of max edit dist 1, offset is at what position the fixed barcode starts XXXTTGTXX_READ_ > primer XXACAAXXX
+    echo "Remove barcodes and pick out data"
+    cd $outFolder
+    cat $FILE_NAME/$FILE|$scripts/get_data_from_barcode.py -p $barcode -o 3 -m 1 1> $outFolder/$ftmp
+    cd ..
+else
+    echo "Data has no fixed barcodes"
+    cp $FILE_NAME/$FILE $outFolder/$ftmp
+fi
+echo "adapter length" ${#2}
+if [ ${#2} -gt 0 ]
+then
+    # Remove Adaptors
+    echo "Remove Adaptors"
+    adapterS=$2
+    cd $outFolder
+    $scripts/adapterHMM_multiPY.py -q $Qtype -s $adapterS -f $outFolder/$ftmp -o $outFolder/$f2 -l $(($length+$trim)) -p $threads
+    rm *_adaptor_hmm.imod
+    rm $outFolder/$ftmp
+    cd ..
+else
+    echo "Don't remove Adaptors"
+    mv $outFolder/$ftmp $outFolder/$f2
+fi
+# Remove duplicates
+if [ $6 -eq 1 -o $6 -eq 4 ]
+then
+    echo "Remove duplicates with random barcodes"
+else
+    echo "Remove duplicates without random barcodes"
+    trim=0
+fi
+
+if [ $5 -eq 1 ]
+then
+    cd $outFolder
+    $scripts/duplication_barcode.py -f $outFolder/$f2 -t $trim > $outFolder/$f3
+    cd ..
+else
+    mv $outFolder/$f2 $outFolder/$f3
+fi
+rm $outFolder/$f2
+
+# cut suffix .fastq
+
+map_reads=${f3%.*}
+map_exon=$map_reads.ej
+echo "Map"
+if [ ${10} -eq 0 ]
+then
+    # Map with error model
+    $bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt -G $error_model $idx1 $outFolder/$map_reads.fastq  > $outFolder/$map_reads.sai
+    $bwa samse -f $outFolder/$map_reads.sam $idx1 $outFolder/$map_reads.sai $outFolder/$map_reads.fastq
+    rm $outFolder/$map_reads.sai
+    cat $outFolder/$map_reads.sam|$scripts/read_stats.py -t $PP 1> $outFolder/$map_reads.mappingstats.txt
+else
+    $bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt $idx1 $outFolder/$map_reads.fastq  > $outFolder/$map_reads.sai
+    $bwa samse -f $outFolder/$map_reads.sam $idx1 $outFolder/$map_reads.sai $outFolder/$map_reads.fastq
+    rm $outFolder/$map_reads.sai
+
+    cat $outFolder/$map_reads.sam|$scripts/read_stats.py -t $PP 1> $outFolder/$map_reads.mappingstats.txt
+fi
+if [ $9 -eq 2 ]
+then
+    # Exon-junction mapping
+    $scripts/select_unmapped_reads.pl -f $outFolder/$map_reads.fastq -s $outFolder/$map_reads.sam  > $outFolder/$map_exon.fastq
     
-    if [ $6 -eq 1 -o $6 -eq 3 ]
-    then
-	# Remove barcode and select data of max edit dist 1, offset is at what position the fixed barcode starts XXXTTGTXX_READ_ > pirmer XXACAAXXX
-	echo "Remove barcodes and pick out data"
-	cd $outFolder
-	cat $FILE_NAME/$FILE|$scripts/get_data_from_barcode.py -p $barcode -o 3 -m 1 1> $outFolder/$ftmp
-	cd ..
-    else
-	echo "Data has no fixed barcodes"
-	cp $FILE_NAME/$FILE $outFolder/$ftmp
-    fi
-    echo "adapter length" ${#2}
-    if [ ${#2} -gt 0 ]
-    then
-	# Remove Adaptors
-	echo "Remove Adaptors"
-	adapterS=$2
-	cd $outFolder
-	$scripts/adapterHMM_multiPY.py -q $Qtype -s $adapterS -f $outFolder/$ftmp -o $outFolder/$f2 -l $(($length+$trim)) -p $threads
-	cd ..
-    else
-	echo "Don't remove Adaptors"
-	mv $outFolder/$ftmp $outFolder/$f2
-    fi
-    
-    # Remove duplicates
-    if [ $6 -eq 1 -o $6 -eq 4 ]
-    then
-	echo "Remove duplicates with random barcodes"
-    else
-	echo "Remove duplicates without random barcodes"
-	trim=0
-    fi
-    
-    if [ $5 -eq 1 ]
-    then
-	cd $outFolder
-	$scripts/duplication_barcode.py -f $outFolder/$f2 -t $trim > $outFolder/$f3
-	cd ..
-    else
-	mv $outFolder/$f2 $outFolder/$f3
-    fi
-    
-    # cut suffix .fastq
-    # FILE=${FILE%.*}
-    map_reads=${FILE%.*}
-    map_exon=$map_reads.ej
-    echo "Map"
     if [ ${10} -eq 0 ]
-    then
-	# Map with error model
-	$bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt -G $BASE/cluster/error_model_125.txt $idx1 $FILE_NAME/$map_reads.fastq  > $outFolder/$map_reads.sai
+    then	
+	$bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt -G $error_model $idx3 $outFolder/$map_exon.fastq > $outFolder/$map_exon.sai
     else
-	$bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt $idx1 $FILE_NAME/$map_reads.fastq  > $outFolder/$map_reads.sai
-	$bwa samse -f $outFolder/$map_reads.sam $idx1 $outFolder/$map_reads.sai $FILE_NAME/$map_reads.fastq
-	$scripts/mappingStats.sh $outFolder/$map_reads.sam $PP y > $outFolder/$map_reads.mappingstats.txt
+	$bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt $idx3 $outFolder/$map_exon.fastq > $outFolder/$map_exon.sai
     fi
-    if [ $9 -eq 2 ]
-    then
-	# Exon-junction mapping
-	$BASE/scripts/select_unmapped_reads.pl -f $FILE_NAME/$map_reads.fastq -s $outFolder/$map_reads.sam  > $outFolder/$map_exon.fastq
-	
-	if [ ${10} -eq 0 ]
-	then	
-	    $bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt -G $BASE/cluster/error_model_125.txt $idx3 $outFolder/$map_exon.fastq > $outFolder/$map_exon.sai
-	else
-	    $bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt $idx3 $outFolder/$map_exon.fastq > $outFolder/$map_exon.sai
-	fi
-	$bwa samse -f $outFolder/$map_exon.sam $idx3 $outFolder/$map_exon.sai $outFolder/$map_exon.fastq
-	$scripts/mappingStats.sh $outFolder/$map_exon.sam $PP y > $outFolder/$map_exon.mappingstats.txt
-    fi
+    $bwa samse -f $outFolder/$map_exon.sam $idx3 $outFolder/$map_exon.sai $outFolder/$map_exon.fastq
+    rm $outFolder/$map_exon.sai
+    cat $outFolder/$map_exon.sam |$scripts/read_stats.py -t $PP 1> $outFolder/$map_exon.mappingstats.txt
+fi
 #done
 
-    if [ $9 -ne 2 ] # remove
-    then
-	
-	# Make bed-files 
-	# Genome
-	$BASE/scripts/parse_sam_files_final.pl -p $idx1 -t pssm -c $PP -f $outFolder/$map_reads.sam -o $outFolder/$map_reads.bed -a
-    fi
-    if [ $9 -eq 2 ]
-    then
-	# Exon junctions
-	$BASE/scripts/parse_sam_files_ej_final.pl -p $idx3 -t pssm -c $PP -f $outFolder/$map_exon.sam -o $outFolder/$map_exon.bed -a
-    fi
+    # Make bed-files 
+# Genome
+$BASE/scripts/parse_sam_files_final.pl -p $idx1 -t pssm -c $PP -f $outFolder/$map_reads.sam -o $outFolder/$map_reads.bed -a
+if [ $9 -eq 2 ]
+then
+    # Exon junctions
+    $BASE/scripts/parse_sam_files_ej_final.pl -p $idx3 -t pssm -c $PP -f $outFolder/$map_exon.sam -o $outFolder/$map_exon.bed -a
+fi
 # peak Calling
 exon_annot=$BASE"/resources/ensembl70.all.long_nooverlap.txt"
 #exon_annot="/seqdata/krogh/mplass/RBPs/FDR/ensembl70.long.exons.txt"
-
-
-
-if [ ${14} -ne 0 ]
-then
-    if [ $8 -eq 0 ]
-    then
-	$pyicos -F 'bed_pk' --region $exon_annot --stranded $outFolder/$peak_in $outFolder/$peak_out 
-    else
-	# not stranded
-	$pyicos -F 'bed_pk' --region $exon_annot $outFolder/$peak_in $outFolder/$peak_out 
-    fi
-    #    # not stranded
-fi
 
 if [ $9 -eq 1 ]
 then
@@ -268,53 +258,63 @@ then
     peak_out=$map_reads.pk
     # rename genome reads file
     cat $outFolder/$map_reads.bed  > $outFolder/$final_bed
-fi
-
-if [ $9 -eq 2 ]
-then
-    # exon junctions
-    peak_in=$map_exon.bed
-    peak_out=$map_exon.pk
     if [ ${14} -ne 0 ]
     then
 	if [ $8 -eq 0 ]
 	then
-	    $pyicos -f 'bed' -F 'bed_pk' --region $exon_annot --stranded $outFolder/$peak_in $outFolder/$peak_out 
+	    $pyicos -f 'bed' -F 'bed_pk' --p-value 0.01 --region $exon_annot --stranded $outFolder/$peak_in $outFolder/$peak_out 
 	else
 	    # not stranded
-	    $pyicos -f 'bed' -F 'bed_pk' --region $exon_annot $outFolder/$peak_in $outFolder/$peak_out 
+	    $pyicos -f 'bed' -F 'bed_pk'  --p-value 0.01 --region $exon_annot $outFolder/$peak_in $outFolder/$peak_out 
+	fi
+	#    # not stranded
+    fi
+fi
+
+if [ $9 -eq 2 ]
+then
+    # Genome reads
+    peak_in=$map_reads.both.bed
+    peak_out=$map_reads.both.pk
+    
+    # exon junctions
+    #peak_in2=$map_exon.bed
+    #peak_out2=$map_exon.pk
+
+    cat $outFolder/$map_reads.bed $outFolder/$map_exon.bed > $outFolder/$peak_in
+     
+    if [ ${14} -ne 0 ]
+    then
+	if [ $8 -eq 0 ]
+	then
+	    $pyicos -f 'bed' -F 'bed_pk' --p-value 0.01 --region $exon_annot --stranded $outFolder/$peak_in $outFolder/$peak_out 
+	else
+	    # not stranded
+	    $pyicos -f 'bed' -F 'bed_pk' --p-value  0.01 --region $exon_annot $outFolder/$peak_in $outFolder/$peak_out 
 	fi
 	#    # not stranded
     fi
     
     # Merge reads from genome and exon junctions
-    cat $outFolder/$map_reads.bed $outFolder/$map_exon.bed > $outFolder/$final_bed
+    cat $outFolder/$peak_in
+    cat $outFolder/$peak_out
 fi
 
 if [ $7 -eq 1 ]
 then
-    #    echo "Printing UCSC custom tracks..."
-    cat $clusters |sort -s -k6,6 -k1,1 -k2,2g -k3,3g | $scripts/cluster_reads.py -2 -u -w - 1> /dev/null 2> "UCSC_-_"${f1%.*}".track"
-    cat $clusters |sort -s -k6,6 -k1,1 -k2,2g -k3,3g | $scripts/cluster_reads.py -2 -u -w + 1> /dev/null 2> "UCSC_+_"${f1%.*}".track"
     if [ $8 -eq 1 ]
     then
 	echo "Printing UCSC custom tracks for stranded protocol..."
-	cat $outFolder/$final_bed|sort -s -k6,6 -k1,1 -k2,2g -k3,3g|$scripts/cluster_reads.py -2 1> $outFolder/$clusters
-	cat $outFolder/$clusters | $scripts/pk2bedGraph_info.pl -c 1,2,7,6 -s - -n "-_strand_"$name -d "Clusters on minus-strand" > $outFolder/"UCSC_m_"${f1%.*}".track"
-	cat $outFolder/$clusters | $scripts/pk2bedGraph_info.pl -c 1,2,7,6 -s + -n "+_strand_"$name -d  "Clusters on plus-strand" > $outFolder/"UCSC_p_"${f1%.*}".track"
+	cat $outFolder/$peak_out|$scripts/pk2bedGraph_info.pl -c 1,2,4,6 -s - -n "-_strand_"$name -d "Clusters on minus-strand" > $outFolder/"UCSC_m_"${FILE%.*}".track"
+	cat $outFolder/$peak_out | $scripts/pk2bedGraph_info.pl -c 1,2,4,6 -s + -n "+_strand_"$name -d  "Clusters on plus-strand" > $outFolder/"UCSC_p_"${FILE%.*}".track"
 	
-	gzip $outFolder/"UCSC_m_"${f1%.*}".track"
-	gzip $outFolder/"UCSC_p_"${f1%.*}".track"
+	gzip $outFolder/"UCSC_m_"${FILE%.*}".track"
+	gzip $outFolder/"UCSC_p_"${FILE%.*}".track"
     else
 	echo "Printing UCSC custom track for strandless protocol..."
-	cat $outFolder/$final_bed |sort -s -k1,1 -k2,2g -k3,3g | $scripts/cluster_reads.py -s "+" -2 1> $outFolder/$clusters
-	cat $outFolder/$clusters | $scripts/pk2bedGraph_info.pl -c 1,2,7,6 -s "+" -n $name -d "Clusters on both strands" > $outFolder/"UCSC_mp_"${f1%.*}".track"
-	
-	gzip $outFolder/"UCSC_-+_"${f1%.*}".track"
+	cat $outFolder/$peak_out | $scripts/pk2bedGraph_info.pl -c 1,2,4,6 -s "+" -n $name -d "Clusters on both strands" > $outFolder/"UCSC_mp_"${FILE%.*}".track"
+	gzip $outFolder/"UCSC_-+_"${FILE%.*}".track"
     fi
-else
-    echo "Do not print UCSC custom tracks..."
-    cat $outFolder/$final_bed |sort -s -k6,6 -k1,1 -k2,2g -k3,3g | $scripts/cluster_reads.py -2 1> $outFolder/$clusters
 fi
 
 #done
