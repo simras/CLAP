d4bad84ce723a2a35c8c0ce931a8bcef22795985
simras <simras@binf.ku.dk>
2017-02-07 21:22:34 +0100

    Add test file

--- scripts/testCLAP.sh
+++ scripts/testCLAP.sh
@@ -7,10 +7,15 @@
 #  University of Copenhagen
 #
 ######################################################################################
+#  Dataset: Reads that map to chr4 from the PAR-CLIP dataset SRR248532
+#
+######################
 
 BASE=$(pwd)
 file7=$BASE"/resources/SRR248532_chr4.fastq"
 adapt="TCGTATGCCGTCTTCTGCTTG"
 
-# Run the pipeline
+#  Run the pipeline
+#  Should be run from the CLAP/ base Directory
+
 scripts/CLAP.sh $file7 $adapt "" 0 1 2 1 1 2 0 test_CLAP 1 8 1
