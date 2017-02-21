8cb9b35de96aa83c7a4392249495f36bbf25e8dc
simras <simras@binf.ku.dk>
2017-02-21 12:16:58 +0100

    Add script for processing annotation.

---
+++ scripts/make_exon_junction_library.pl
@@ -0,0 +1,129 @@
+#!/usr/bin/perl -w
+use strict;
+
+if (@ARGV != 2){
+    print "introduce the annotation file and the genome fasta file\n";
+    print "The exon junction  library will be printed to standard out\n";
+
+    print "USAGE: ./make_exon_junction_library.pl annotation.all.txt hg19.fa\n\n";
+    exit (1);
+}
+
+## save exons
+my %tr;
+
+open (ARX, "<$ARGV[0]") || die ("cannot open $ARGV[0] for reading");
+while (<ARX>){
+    chomp;
+    my @line = split;
+    my @c = @line[1..2];
+    ## {chr}{strand}{gene}{transcript} (exon1, exon2, ...)
+    push (@{$tr{$line[0]}{$line[3]}{$line[6]}{$line[7]}}, \@c);
+}
+close (ARX);
+
+## define pairs of exons
+
+my %junctions;
+
+foreach my $chr (keys %tr){
+    foreach my $strand (keys %{$tr{$chr}}){
+	foreach my $gene (keys %{$tr{$chr}{$strand}}){
+	    foreach my $tr (keys %{$tr{$chr}{$strand}{$gene}}){
+		my @exons = sort {$a->[0] <=> $b->[0]} @{$tr{$chr}{$strand}{$gene}{$tr}};
+		next if (@exons < 2);
+		for (my $i = 0; $i < @exons-1; $i++){
+		    my @e1 = ($exons[$i][0],$exons[$i][1]);
+		    my @e2 = ($exons[$i+1][0],$exons[$i+1][1]);
+		    if ($exons[$i][1]-$exons[$i][0] >= 99){
+			@e1 = ($exons[$i][1]-99,$exons[$i][1]);
+		    }
+		    if ($exons[$i+1][1]-$exons[$i+1][0] >= 99){
+			@e2 = ($exons[$i+1][0],$exons[$i+1][0]+99);
+		    }
+		    my @pair = (@e1,@e2);
+		    my $jun = "$chr:$strand:$e1[1]:$e2[0]";
+		    push (@{$junctions{$jun}},\@pair);
+		}
+	    }
+	}
+    }
+}
+
+# get the sequence of the exon pairs and print it to a multifasta file
+
+my $genome_file = $ARGV[1];
+my %final_set;
+my @final_ids;
+my %segments;
+
+my $bedfile="annotation".$$.".bed";
+open (T, ">$bedfile") || die ("cannot open $bedfile for writing");
+
+foreach my $id (keys %junctions){
+    my ($chr,$strand,$s,$e) = split (":", $id);
+    my @pairs = @{$junctions{$id}};
+    my @segments=@{$pairs[0]};
+    if (@pairs > 1){
+	for (my $i = 1; $i < @pairs; $i++){
+	    if ($pairs[$i][0] < $segments[0]){
+		$segments[0] = $pairs[$i][0];
+	    }
+	    if ($pairs[$i][3] > $segments[3]){
+		$segments[3] = $pairs[$i][3];
+	    }
+	}
+    }
+
+    $chr=~s/MT/M/;
+    print T join ("\t", $chr,$segments[0],$segments[1],".",".",$strand), "\n";
+    print T join ("\t", $chr,$segments[2],$segments[3],".",".",$strand), "\n";
+    
+    my $id1=join ("_", $chr,$segments[0],$segments[1],".",".",$strand);
+    my $id2=join ("_", $chr,$segments[2],$segments[3],".",".",$strand);
+
+    push (@final_ids, $id1,$id2);
+
+    my $junction_id =join (":",$chr,$strand,@segments);
+    if ($strand eq "-"){
+	@{$final_set{$id}} = ($id2,$id1);
+    }
+    else{
+	@{$final_set{$id}} = ($id1,$id2);
+    }
+}
+close (T);
+
+
+my $seqfile= "out.".$$.".tab";
+my @seqs;
+my $command = "bedtools getfasta -fi $genome_file -bed $bedfile -tab -fo $seqfile -s";
+
+system ("$command");
+unlink ($bedfile);
+
+open (SEQ, "<$seqfile") || die ("cannot open $seqfile for reading");
+while (<SEQ>){
+    chomp;
+    my @line = split;
+    push (@seqs, uc($line[-1]));
+}
+close (SEQ);
+
+### join name and sequence
+for (my $i = 0; $i < @seqs; $i++){
+    $segments{$final_ids[$i]} = $seqs[$i];
+}
+
+## print out fasta file 
+foreach my $k (keys %final_set){
+    my $seq1 = $segments{$final_set{$k}[0]};
+    my $seq2 = $segments{$final_set{$k}[1]};
+
+    print ">$k\n$seq1$seq2\n";
+}
+
+		     
+
+
+  
---
+++ scripts/read_stats.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# 
+# 
+# 
+
+def parse_chr(ID,rel_start):
+    my_chr,my_str,my_st,my_end = ID.split(":")
+    #print my_chr,my_str,my_st,my_end
+    my_st = int(my_st)
+    my_end = int(my_end)
+    my_len = my_end-my_st
+    #my_st = .split(":")[0]
+    #print my_chr, my_str,my_st
+    if my_str == "-":
+        my_start = int(my_st) + rel_start
+    else:
+        my_start = int(my_st) - rel_start + my_len
+    #print  my_chr, my_start, my_str    
+    return my_chr, my_start, my_str
+
+def parse_chr2(ID,rel_start):
+    my_chr,my_str,my_st,my_end = ID.split(":")
+    my_st = int(my_st)
+    my_end = int(my_end)
+    my_len = my_end-my_st
+
+    if my_str == "-":
+        my_start = int(my_st) +  my_len - rel_start
+    else:
+        my_start = int(my_st) + rel_start #+ my_len
+    return my_chr, my_start, my_str
+
+def parse_chr3(ID,rel_start,my_str,r_len):
+    try:
+        my_chr,my_st,my_end,my_str = ID.split(":")
+        if my_st == "+" or my_st == "+" :
+            my_chr,my_str,my_st,my_end = ID.split(":")
+        my_st = int(my_st)
+        my_start = my_st + rel_start
+    except:
+        my_chr = ID
+        my_start = rel_start
+    return my_chr, my_start, my_str
+
+def filter_reads(fname,t,t2,print_IDs):
+    import sys
+    u = 0
+    m = 0
+    cp = 0
+    cm = 0
+    mp = 0
+    mm = 0
+    i = 0
+    a = 0
+    for l in sys.stdin:
+        
+        if l[0] == "@":
+            continue
+        PP = 0
+        a = a + 1
+        try:
+            ID = l.split()
+            read_ID = ID[0]
+            bit = int(ID[1])
+            r_len = int(len(ID[9]))
+
+            #  Only check certain bit-flags
+                    
+            # Exon location
+            Exon_loc = ID[2]
+            rel_start = int(ID[3])
+
+            try:
+                m = m + 1
+                rest = l.split("PP:f:")[1]
+                PP = float(rest.split()[0])
+            except:
+                PP=0
+            revbit = bin(bit)[::-1]
+            
+            if int(revbit[2]) == 1:
+                # Unmapped reads
+                u = u + 1
+            elif bit == 0:
+                # Mapped to the plus strand
+                if PP > t:
+                    # confidently mapped
+                    cp = cp + 1        
+                else:
+                    # multimapped
+                    mp = mp + 1
+            elif int(revbit[4]) == 1:
+                # Mapped to the minus strand
+                if PP > t:
+                    # confidently mapped
+                    cm = cm + 1
+                else:
+                    # multimapped
+                    mm = mm + 1
+            else:
+                print >>sys.stderr, "Warning: Unexpected input"
+        except:
+            None
+    try:
+        print >>sys.stderr, "Confidently mapped ",float(cm + cp)/a,"Confidently mapped plus ",float(cp)/a,"Confidently mapped minus ",float(cm)/a, " Multiple mapped ",float(mm + mp)/a," unmapped ",float(u)/a
+        print "Confidently_Mapped_Minus","Confidently_Mapped_Plus","Multiple_Mapped_Minus","Multiple_Mapped_Plus", "Unmapped_reads","All_reads"
+        print cm,cp,mp,mm,u,a
+        if cm + cp + mm + mp + u != a:
+            print >>sys.stderr, "Warning: ",a-(cm+cp+mm+mp+u)," reads were not counted."
+    except ZeroDivisionError:
+        
+        print >>sys.stderr, "No reads mapped"
+        print "No reads mapped"
+        print cm,cp,mp,mm,u,a
+        if cm + cp + mm + mp + u != a:
+            print >>sys.stderr, "Warning: ",a-(cm+cp+mm+mp+u)," reads were not counted."
+
+if __name__ == "__main__":                                                                                                                                         
+    from optparse import OptionParser  
+    parser = OptionParser()
+    
+    parser.add_option("-f", action="store", type="string", dest="f",default="", help="Input file.")
+    parser.add_option("-t", action="store", type="float", dest="t",default="0.99", help="PP Threshold conf map vs. mult map")
+    parser.add_option("-l", action="store", type="float", dest="l",default="0.01", help="PP low bound")
+    parser.add_option("-m", action="store", type="int", dest="m",default="1", help="Mode 1: filter reads, 2: select reads and 3: count correctly mapped.")
+    parser.add_option("-i", action="store_true", dest="i",default=False, help="Print IDs of selected reads.")
+    
+    (options, args) = parser.parse_args()
+        
+#    countR(options.f,options.t,options.e,options.l,options.s)
+    filter_reads(options.f,options.t,options.l,options.i)
