5102984855da9fbeaefaa906464582831860839d
simras <simras@gmail.com>
2017-03-17 15:06:31 +0100

    implemented timestamps

--- scripts/CLAP.sh
+++ scripts/CLAP.sh
@@ -2,9 +2,13 @@
 #
 #  CLAP - CLIP Analysis Pipeline
 # 
-#  Simon H. Rasmussen and Mireya Plass
+#  Simon H. Rasmussen
 #  Bioinformatics Centre
 #  University of Copenhagen
+#
+#  Mireya Plass
+#  Max Delbrück Center
+#  Berlin, Germany
 #
 ####################################################################################################
 ####################################################################################################
@@ -13,13 +17,14 @@
 #stuff
 #end=`date +%s`
 #
-#runtime=$((end-start))
+#runtime=$((`date +%s`-start))
 #
 ############
+start=`date +%s`
 echo "#################### CLAP - CLIP Analysis Pipeline ####################"
 echo "#######################################################################"
 echo "#######################################################################"
-echo
+
 
 trap "" 1
 if [ $# -lt 14 ]
@@ -167,7 +172,7 @@ else
 fi
 
 mkdir $outFolder
-trap 'echo "###################### Output directory: "' 0
+trap "echo '###################### Output directory: '" 0
 trap "echo $outFolder" 0
 
 #exec 1> $outFolder/mapping_pipe.log
@@ -210,11 +215,13 @@ then
     echo
     echo "#################### Removing barcodes and pick out data "
     
+
     cd $outFolder
     cat $FILE_NAME/$FILE|$scripts/get_data_from_barcode.py -p $barcode -o 3 -m 1 1> $outFolder/$ftmp
     cd ..
 else
     echo "#################### Data has no fixed barcodes ##"
+
     cp $FILE_NAME/$FILE $outFolder/$ftmp
 fi
 
@@ -223,6 +230,7 @@ then
     # Remove Adaptors
    
     echo "#################### Removing Adapters "
+    echo "#################### Runtime " $((`date +%s`-start))
     echo
     adapterS=$2
     cd $outFolder
@@ -233,6 +241,7 @@ then
 else
     echo
     echo "#################### Will not remove Adaptors "
+    echo "#################### Runtime " $((`date +%s`-start))
     echo
     mv $outFolder/$ftmp $outFolder/$f2
 fi
@@ -241,10 +250,12 @@ if [ $6 -eq 1 -o $6 -eq 4 ]
 then
     echo
     echo "#################### Remove duplicates using random barcodes "
+    echo "#################### Runtime " $((`date +%s`-start))
     echo
 else
     echo
     echo "#################### Remove duplicates without using random barcodes "
+    echo "#################### Runtime " $((`date +%s`-start))
     echo
     trim=0
 fi
@@ -270,12 +281,14 @@ if [ ${10} -eq 0 ]
 then
 
 echo "#################### Use substituion model " 
+    echo "#################### Runtime " $((`date +%s`-start))
 echo
     # Map with error model
     $bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt -G $error_model $idx1 $outFolder/$map_reads.fastq  > $outFolder/$map_reads.sai
 else
 
 echo "#################### Do not use substitution model " 
+    echo "#################### Runtime " $((`date +%s`-start))
 echo
     $bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt $idx1 $outFolder/$map_reads.fastq  > $outFolder/$map_reads.sai
 #    $bwa samse -f $outFolder/$map_reads.sam $idx1 $outFolder/$map_reads.sai $outFolder/$map_reads.fastq
@@ -286,22 +299,26 @@ $bwa samse -f $outFolder/$map_reads.sam $idx1 $outFolder/$map_reads.sai $outFold
 rm $outFolder/$map_reads.sai
 echo
 echo "#################### Genome mapping statistics "
+echo "#################### Runtime " $((`date +%s`-start))
 echo
 cat $outFolder/$map_reads.sam|$scripts/read_stats.py -t $PP 1> $outFolder/$map_reads.mappingstats.txt
 if [ $9 -eq 2 ]
 then
 echo
 echo "#################### Mapping to the exon junctions with bwa-pssm " 
+echo "#################### Runtime " $((`date +%s`-start))
     # Exon-junction mapping
     $scripts/select_unmapped_reads.pl -f $outFolder/$map_reads.fastq -s $outFolder/$map_reads.sam  > $outFolder/$map_exon.fastq
     
     if [ ${10} -eq 0 ]
     then	
 	echo "#################### Use substituion model " 
+	echo "#################### Runtime " $((`date +%s`-start))
 	echo
 	$bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt -G $error_model $idx3 $outFolder/$map_exon.fastq > $outFolder/$map_exon.sai
     else
 	echo "#################### Do not use substitution model " 
+	echo "#################### Runtime " $((`date +%s`-start))
 	echo
 	$bwa pssm -n $E -l $L -m $H -t $T -P $P $phreadOpt $idx3 $outFolder/$map_exon.fastq > $outFolder/$map_exon.sai
     fi
@@ -310,6 +327,7 @@ echo "#################### Mapping to the exon junctions with bwa-pssm "
     
     echo
     echo "#################### Exon junction mapping statistics "
+    echo "#################### Runtime " $((`date +%s`-start))
     echo
     cat $outFolder/$map_exon.sam |$scripts/read_stats.py -t $PP -j 1> $outFolder/$map_exon.mappingstats.txt
 fi
@@ -325,6 +343,7 @@ fi
 # peak Calling
 echo
 echo "#################### Calculate False Discovery rate of read clusters with Pyicosclip " 
+echo "#################### Runtime " $((`date +%s`-start))
 echo
 
 if [ $9 -eq 1 ]
@@ -385,6 +404,7 @@ then
     if [ $8 -eq 1 ]
     then
 	echo "#################### UCSC custom tracks for stranded protocol..."
+	echo "#################### Runtime " $((`date +%s`-start))
 	cat $outFolder/$peak_out|$scripts/pk2bedGraph_info.pl -c 1,2,4,6 -s - -n "-_strand_"$name -d "Clusters on minus-strand" > $outFolder/"UCSC_-_"${FILE%.*}".track"
 	cat $outFolder/$peak_out | $scripts/pk2bedGraph_info.pl -c 1,2,4,6 -s + -n "+_strand_"$name -d  "Clusters on plus-strand" > $outFolder/"UCSC_+_"${FILE%.*}".track"
 	
@@ -392,6 +412,7 @@ then
 	gzip $outFolder/"UCSC_+_"${FILE%.*}".track"
     else
 	echo "#################### UCSC custom track for strandless protocol..."
+	echo "#################### Runtime " $((`date +%s`-start))
 	cat $outFolder/$peak_out | $scripts/pk2bedGraph_info.pl -c 1,2,4,6 -s "+" -n $name -d "Clusters on both strands" > $outFolder/"UCSC_mp_"${FILE%.*}".track"
 	gzip $outFolder/"UCSC_-+_"${FILE%.*}".track"
     fi
